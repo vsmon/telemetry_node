4569f3fbfd6a0171fcc9633e3618ea4997357239
Rodrigo Martins <rodrigo.martins@wyda.com.br>
2020-05-22 11:00:30 -0300

    added cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 require("./database");
 
@@ -10,6 +11,7 @@ class App {
   }
   middlewares() {
     this.server.use(express.json());
+    this.server.use(cors());
   }
   routes() {
     this.server.use(routes);
