3ace92fbbcfe7186677a0b0e76504935ec750019
Rodrigo Martins <rodrigo.martins@wyda.com.br>
2020-06-04 09:35:33 -0300

    added delete controller by id and refactored ajax request

--- src/app/controllers/TelemetryController.js
+++ src/app/controllers/TelemetryController.js
@@ -4,19 +4,30 @@ class TelemetryController {
   async store(req, res) {
     const { temperature, humidity, pressure, altitude } = req.body;
 
-    const city = "sorocaba";
-    const uf = "sp";
+    const city = req.query.city || "sorocaba";
+    const uf = req.query.state || "sp";
     const tokenClima = process.env.TOKEN_CLIMATEMPO;
 
     try {
       const cities = await api.get(
-        `locale/city?name=${city}&state=${uf}&token=${tokenClima}`
+        //`locale/city?name=${city}&state=${uf}&token=${tokenClima}
+        `locale/city`,
+        {
+          params: {
+            name: city,
+            state: uf,
+            token: tokenClima,
+          },
+        }
       );
 
-      const { id: idCidade } = cities.data[0];
-      const weather = await api.get(
-        `weather/locale/${idCidade}/current?token=${tokenClima}`
-      );
+      //const { id: idCidade } = cities.data[0];
+      const [{ id: idCidade }] = cities.data;
+      const weather = await api.get(`weather/locale/${idCidade}/current`, {
+        params: {
+          token: tokenClima,
+        },
+      });
 
       const { name, state, data: externalWeather } = weather.data;
       const {
@@ -41,13 +52,29 @@ class TelemetryController {
     } catch (error) {
       return res
         .status(400)
-        .json({ error: `Ocorreu um erro ao obter dados climatempo: ${error}` });
+        .json({ error: `Ocorreu um erro criar registro: ${error}` });
     }
   }
   async index(req, res) {
     const telemetry = await Telemetry.findAll();
-
     return res.json(telemetry);
+  }
+  async delete(req, res) {
+    const { id } = req.params;
+    try {
+      const telemetry = await Telemetry.findByPk(id);
+      if (!telemetry) {
+        return res
+          .status(400)
+          .json({ error: `Nenhum registro encontrado com o id: ${id}` });
+      }
+      await telemetry.destroy();
+      return res.json(telemetry);
+    } catch (error) {
+      res
+        .status(400)
+        .json({ error: `Ocorreu um erro ao excluir o registro: ${error}` });
+    }
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const routes = new Router();
 routes.get("/", (req, res) => res.json({ ok: true }));
 routes.post("/telemetry", TelemetryController.store);
 routes.get("/telemetry", TelemetryController.index);
+routes.delete("/telemetry/:id", TelemetryController.delete);
 
 module.exports = routes;
