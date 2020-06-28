e230687313b3c0914e134d133c62aaa34e5c99e9
Rodrigo Fonseca Martins <rodrigo.fonsecamartins@gmail.com>
2020-06-27 22:35:23 -0300

    changed instuctions to get ip

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,8 @@ routes.get("/", (req, res) => res.json({ ok: true }));
 routes.get("/externalip", (req, res) => {
   const [, , , remoteIp] = req.connection.remoteAddress.split(":", -15);
   const headerIp = req.headers["x-forwarded-for"].split(",")[0];
-  const externalIp = remoteIp || headerIp;
-  res.json({ externalIp });
+  //const externalIp = remoteIp || headerIp;
+  res.json({ externalIp: headerIp });
 });
 routes.post("/telemetry", TelemetryController.store);
 routes.get("/telemetry", TelemetryController.index);
