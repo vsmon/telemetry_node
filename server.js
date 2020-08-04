24992bf735d7f84318ddb9444171ebe72584733f
Rodrigo Martins <rodrigo.martins@wyda.com.br>
2020-08-04 10:38:05 -0300

    changed server to azure

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const server = require("./src/app");
 
 const port = process.env.PORT || 3000;
 
-server.listen(port, console.log(`Executando na porta ${port}`));
+server.listen(port, () => console.log(`Executando na porta ${port}`));
