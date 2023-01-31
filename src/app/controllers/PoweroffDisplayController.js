25bc33d4a420fe84a6dba85d7b113b7bebbcfb28
Rodrigo <rodrigo.fonsecamartins@gmail.com>
2023-01-30 21:30:54 -0300

    fix time format

--- src/app/controllers/PoweroffDisplayController.js
+++ src/app/controllers/PoweroffDisplayController.js
@@ -8,15 +8,29 @@ class PoweroffDisplayController {
       data: { status_display },
     } = await esp8266Data.get("data");
 
-    const currentTime = new Date().toLocaleTimeString("pt-BR");
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const currentDay = new Date().getDate();
+
+    const currentTime = new Date();
 
     const initialTime = new Date(
-      "2023-01-30T00:30:00.008-03:00"
-    ).toLocaleTimeString("pt-BR");
+      currentYear,
+      currentMonth,
+      currentDay,
+      0o0,
+      30,
+      11
+    );
 
     const finalTime = new Date(
-      "2023-01-30T08:30:00.008-03:00"
-    ).toLocaleTimeString("pt-BR");
+      currentYear,
+      currentMonth,
+      currentDay,
+      11,
+      30,
+      59
+    );
 
     console.log("Current Time: ", currentTime);
     console.log("Intial Time: ", initialTime);
@@ -31,7 +45,11 @@ class PoweroffDisplayController {
         console.log(powerOff.data);
       }
     }
-    if (currentTime === finalTime) {
+
+    if (
+      new Date(currentTime).getHours() === new Date(finalTime).getHours() &&
+      new Date(currentTime).getMinutes() === new Date(finalTime).getMinutes()
+    ) {
       if (status_display === 0) {
         console.log("Liga");
         const powerOn = await esp8266Data.post("/data", {
