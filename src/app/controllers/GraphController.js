0c231496fc96969d5facfbedd510206e0f5dcdfb
Rodrigo <rodrigo.fonsecamartins@gmail.com>
2023-05-27 01:48:20 -0300

    added pagination

--- src/app/controllers/GraphController.js
+++ src/app/controllers/GraphController.js
@@ -1,10 +1,28 @@
 const Telemetry = require("../models/Telemetry");
-class HomeController{
-    async index(req, res) {
-        const telemetry = await Telemetry.findAll();
+class HomeController {
+  async index(req, res) {
+    const size = req.query.size || 72;
 
-        res.render('index.html',{data:JSON.stringify(telemetry)})
-      }
+    const { count: totalItems } = await Telemetry.findAndCountAll();
+
+    const qtPages = Math.ceil(totalItems / size);
+
+    const page = req.query.page || qtPages;
+
+    const telemetry = await Telemetry.findAll({
+      limit: size,
+      offset: (page - 1) * size,
+      order: [["id", "ASC"]],
+    });
+
+    res.render("index.html", {
+      data: JSON.stringify(telemetry),
+      totalItems,
+      page,
+      size,
+      qtPages,
+    });
+  }
 }
 
-module.exports = new HomeController()
+module.exports = new HomeController();
